775d0943c0132148f99ef76829fd0a9bd828a468
fabinho190 <ossai10fabianfbn@gmail.com>
2023-04-21 10:57:34 +0200

    Baking

---
+++ src/main/java/com/example/JpaService.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaService {
+    private static JpaService instance;
+
+    private EntityManagerFactory entityManagerFactory;
+
+    public JpaService() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpa-hibernate-demo-local");
+    }
+
+    public static synchronized JpaService getInstance() {
+        //condicio(if(instance== null)? caso1:caso2)
+        return instance == null ? instance = new JpaService(): instance;
+    }
+
+    public void shutDown(){
+        entityManagerFactory.close();
+    }
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+}
--- src/main/java/com/example/ProgrammingLanguage.java
+++ src/main/java/com/example/ProgrammingLanguage.java
@@ -1,6 +1,64 @@
 package com.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
 public class ProgrammingLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private Integer rating;
+
+    // CONSTRUCTORES
+    public ProgrammingLanguage() {
+    }
+
+    public ProgrammingLanguage(String name, Integer rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProgrammingLanguage that = (ProgrammingLanguage) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
 }
